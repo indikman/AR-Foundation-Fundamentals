949cfce68dce429de3a73b2cba464d42e0d4c9b8
Indika Wijesooriya <indikman@gmail.com>
2021-03-20 22:51:58 +0530

    Basic AR tap to place and plane detection added

---
+++ Assets/Scripts/PlaneToggler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlaneToggler : MonoBehaviour
+{
+    private ARPlaneManager planeManager;
+
+    private void Start()
+    {
+        planeManager = GetComponent<ARPlaneManager>();
+    }
+
+    public void TogglePlanes()
+    {
+        planeManager.enabled = !planeManager.enabled;
+
+        if (planeManager.enabled)
+        {
+            SetPlanesActive(true);
+        }
+        else
+        {
+            SetPlanesActive(false);
+        }
+    }
+
+    private void SetPlanesActive(bool isActive)
+    {
+        foreach(  var plane in planeManager.trackables)
+        {
+            plane.gameObject.SetActive(isActive);
+        }
+    }
+}
